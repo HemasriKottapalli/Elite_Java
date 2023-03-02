327f4a6d03b199a2bf9a346304e145d99784186d
HemasriKottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-03-02 11:26:06 +0530

    Add files via upload
    Day-10: simple task

---
+++ Patterns/HollowSquare.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class HollowSquare {
+
+	public static void main(String[] args) {
+		import java.util.Scanner;
+		public class TwoDArray {
+
+			public static void main(String[] args) {
+				Scanner inp = new Scanner(System.in);
+				System.out.println("Enter n: ");
+				int n = inp.nextInt();
+				for(int i=0; i<n; i++)
+				{
+					for(int j=0; j<n; j++)
+					{
+						if(i==0 || i==(n-1) || j==0 || j==(n-1))
+						{
+							System.out.print("*");
+						}
+
+						else 
+							System.out.print(" ");
+
+					}
+					System.out.println();
+				}
+
+			}
+
+		}
+
+
+	}
+
+}
